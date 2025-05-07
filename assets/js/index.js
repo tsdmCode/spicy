e331edd9cdd21d700b49fa6919e8bee7ccd4ff66
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-05-07 08:04:07 +0200

    Stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,22 @@
+const kontakt = document.getElementById('kontakt-dialog');
 const bigImg = document.getElementById('big-image');
 const thumbnails = document.querySelectorAll('.thumbnail');
+const dialog = document.getElementById('imgDialog');
+const gallery = document.querySelectorAll('.grid-item');
+const dialogImg = document.getElementById('dialogImg');
+
+function openForm(e) {
+  e.preventDefault();
+
+  kontakt.showModal();
+}
+
+gallery.forEach((el) => {
+  el.addEventListener('click', () => {
+    dialog.showModal(); //... åbner vores dialog...
+    dialogImg.src = el.src; //... og sætter src værdien til samme værdi som det billede der blev trykket på!
+  });
+});
 
 thumbnails.forEach((el) => {
   el.addEventListener('click', () => {
@@ -8,3 +25,16 @@ thumbnails.forEach((el) => {
     el.src = temp;
   });
 });
+
+function closeDialog(e) {
+  if (!e.target.contains(dialog)) return;
+  dialog.close();
+}
+
+function closeKontakt(e) {
+  if (!e.target.contains(kontakt)) return;
+  kontakt.close();
+}
+
+document.addEventListener('click', closeDialog);
+document.addEventListener('click', closeKontakt);
